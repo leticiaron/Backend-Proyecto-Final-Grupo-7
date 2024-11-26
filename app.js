d19fe7df356c21ea0bec3b69061a5abd6429f14a
florenciagarciacastelli <florenciagarciacastelli25@gmail.com>
2024-11-26 13:48:47 -0300

    Actualizacion

--- app.js
+++ app.js
@@ -112,3 +112,23 @@ app.post("/login", (req, res) => {
 app.listen(port, () => {
   console.log(`Servidor corriendo en localhost: ${port}`);
 });
+
+//MIDDLEWARE
+const autorizacionMiddleware = (req, res, next) => {
+const token = req.headers.authorization;
+if (!token) {
+return res.status(401).json({ message: "Token de autorización no proporcionado" });
+}
+jwt.verify(token, SECRET_KEY, (err, decoded) => {
+if (err) {
+return res.status(401).json({ message: "Token de autorización inválido" });
+}
+req.user = decoded;
+next();
+});
+};
+// Endpoint protegido que requiere autorización
+app.get("/ruta-protegida", autorizacionMiddleware, (req, res) => {
+// Lógica de la ruta protegida aquí
+  res.json({ message: "Acceso autorizado a la ruta protegida" });
+});
